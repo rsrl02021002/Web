b89111d30051e6c058541c7df0cfd5b3d4c9849a
rsrl02021002 <1052657260@qq.com>
2020-02-15 09:40:40 +0800

    寒假

---
+++ ES6/babel/src/index.js
@@ -0,0 +1,10 @@
+let a = "hello";
+class Person{
+  constructor(name,age){
+    this.name = name;
+    this.age =age
+  }
+  showInfo(){
+    console.log(this.name);
+  }
+}
---
+++ vuecli/my-project/src/views/MyPage.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h1>我的测试页面！</h1>
+    <p class="active">{{str}}</p>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      str:"fello37"
+    }
+  },
+  methods: {
+    
+  },
+}
+</script>
+<style scoped>
+.active{
+  color:red
+}
+</style>
